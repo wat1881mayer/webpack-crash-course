c6999da1c7c1d84ad95ad1ccbc61a3f8293e5353
wat1881mayer <happymayer2013@icloud.com>
2020-12-31 19:01:17 +0900

    eslint setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,70 @@
-const path = require('path')
-const HtmlWebPackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-const outputPath = path.resolve(__dirname,'dist')
-console.log({outputPath})
+const outputPath = path.resolve(__dirname, 'dist');
+console.log({ outputPath });
 
 module.exports = {
-    entry:'./src/index.js',
-    output:{
-        filename:'main.js',
-        path:outputPath
-    },
-    module:{
-        rules:[
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                  loader: "babel-loader"
-              },
-            {
-                test: /\.(jpe?g|png|git|svg|ico)$/i,
-                loader:'url-loader',
-                options: {
-                    limit: 2048,
-                    name: './images/[name].[ext]'
-                }
-            },
-            {
-                test: /\.(sc|c)ss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test:/\.html?/,
-                loader:'html-loader'
-            }
-        ]
-    },
-    devServer:{
-        contentBase:outputPath
-    },
-    plugins:[
-        new HtmlWebPackPlugin({
-            template:'./src/index.html',
-            filename:'./index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].[hash].css'
-        }) 
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: outputPath,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(jpe?g|png|git|svg|ico)$/i,
+        loader: 'url-loader',
+        options: {
+          limit: 2048,
+          name: './images/[name].[ext]',
+        },
+      },
+      {
+        test: /\.(sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.html?/,
+        loader: 'html-loader',
+      },
     ],
-    optimization:{
-        minimizer:[new UglifyJsPlugin({
-            uglifyOptions:{
-                compress:{
-                    drop_console:true
-                }
-            }
-        }),
-        //OptimizeCssAssetsPlugin
-        new OptimizeCSSAssetsPlugin({})
-    ]
-    },
-    devtool:'eval-source-map'
-}
+  },
+  devServer: {
+    contentBase: outputPath,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css',
+    }),
+  ],
+  optimization: {
+    minimizer: [new UglifyJsPlugin({
+      uglifyOptions: {
+        compress: {
+          drop_console: true,
+        },
+      },
+    }),
+    // OptimizeCssAssetsPlugin
+    new OptimizeCSSAssetsPlugin({}),
+    ],
+  },
+  devtool: 'eval-source-map',
+};
